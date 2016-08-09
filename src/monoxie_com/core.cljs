36efcb6bc97b417b985f943271981a91f0b882e9
Daniel Berg <mail@roosta.sh>
2016-08-09 21:27:56 +0200

    Add snapsvg, load resource.

--- src/monoxie_com/core.cljs
+++ src/monoxie_com/core.cljs
@@ -1,19 +1,60 @@
 (ns monoxie-com.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require 
+    [reagent.core :as r :refer [atom]]
+     [garden.units :as u]
+     [cljsjs.snapsvg :as snap]
+     [garden.core :as g]
+     [goog.labs.userAgent.device :as device]
+     [garden.selectors :as s]
+     [garden.stylesheet :as stylesheet]
+     ))
 
 (enable-console-print!)
 
-(println "This text is printed from src/monoxie-com/core.cljs. Go ahead and edit it and see reloading in action.")
-
 ;; define your app data so that it doesn't get over-written on reload
-
 (defonce app-state (atom {:text "Hello world!"}))
 
-(defn hello-world []
-  [:h1 (:text @app-state)])
+(defn svg-init []
+  (let [svg (js/Snap. "#svg")]
+    (.load js/Snap "/svg/splotshy.svg"
+           (fn [el]
+             (.append svg el)
+             ))
 
-(reagent/render-component [hello-world]
-                          (. js/document (getElementById "app")))
+    ))
+
+(def style
+  (g/css
+    [:body
+     {:background-color "#1C1B19"
+      }]
+    [:#wrapper
+     {:display "flex"
+      :height "100vh"
+      :justify-content "center"
+      :align-items "center"}]
+    [:#svg 
+     ])
+  )
+
+(defn Page []
+  ; (let [timer (timer. (rand-int 10000))]
+    (r/create-class
+     {:display-name "monoxie.com"
+      :component-did-mount #(svg-init)
+      ; :component-will-unmount #(.dispose timer)
+      ; :component-did-mount #(.init js/smoothscroll)
+      ; :component-will-unmount #()
+      :reagent-render
+      (fn []
+        [:div#wrapper
+         [:style style]
+         [:svg#svg {:width 322 :height 552 :viewBox "0 0 322 552"}]]
+        )}))
+
+
+(r/render-component [Page]
+                    (. js/document (getElementById "app")))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
